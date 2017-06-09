be0ead4c1afc811678502fe62e46e2c9f9f2000f
mhoppal <michael.jam.hoppal@hpe.com>
2017-06-09 16:08:44 -0600

    Initial commit

---
+++ smoke-test.go
@@ -0,0 +1,174 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"time"
+	"github.com/gophercloud/gophercloud/openstack"
+	"os"
+	"fmt"
+	"github.com/gophercloud/gophercloud"
+	"github.com/monasca/golang-monascaclient/monascaclient"
+	"strconv"
+	"github.com/monasca/golang-monascaclient/monascaclient/models"
+)
+
+var webhookTriggered bool = false
+
+func handleWebhook(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Received WEBHOOK")
+	webhookTriggered = true
+}
+
+func getToken(authOptions *gophercloud.AuthOptions) (string, error) {
+	openstackProvider, err := openstack.AuthenticatedClient(*authOptions)
+	if err != nil {
+		return "", err
+	}
+	return openstackProvider.TokenID, nil
+}
+
+func initializeMonascaClient(token, monascaURL string)(){
+	timeoutSetting := os.Getenv("TIMEOUT")
+	var timeout int
+	if timeoutSetting == "" {
+		timeout = 5
+	} else {
+		var err error
+		timeout, err = strconv.Atoi(timeoutSetting)
+		if err != nil {
+			timeout = 5
+		}
+	}
+	monascaclient.SetBaseURL(monascaURL)
+	monascaclient.SetTimeout(timeout)
+	headers := http.Header{}
+	headers.Add("X-Auth-Token", token)
+	monascaclient.SetHeaders(headers)
+}
+
+func testMeasurementsFlowing(){
+	//mergeMetrics := false
+	metricName := "pod.cpu.total_time_sec"
+	startTime := time.Now()
+	startTime = startTime.Add(-3 * time.Minute)
+	groupBy := "*"
+	measurementQuery := models.MeasurementQuery{Name:&metricName, StartTime:&startTime, GroupBy:&groupBy}
+	measurements, err := monascaclient.GetMeasurements(&measurementQuery)
+	if err != nil {
+		fmt.Printf("FAILED - Error getting measurements from API test failed %s\n", err.Error())
+	}
+	if len(measurements.Elements) == 0 {
+		fmt.Println("FAILED - No current measurements found for pod.cpu.total_time_sec")
+	}
+	fmt.Println("SUCCESS")
+}
+
+func testCreateNotification(webhookAddress string) string{
+	name := "smoke_test_notification"
+	notificationType := "webhook"
+	requestBody := models.NotificationRequestBody{Name:&name, Type:&notificationType, Address:&webhookAddress}
+	notificationResponse, err := monascaclient.CreateNotificationMethod(&requestBody)
+	if err != nil {
+		fmt.Printf("FAILED - Error creating notification method %s\n", err.Error())
+		return ""
+	}
+	fmt.Println("SUCCESS")
+	return notificationResponse.ID
+}
+
+func testCreateAlarmDefinition(notificationID string) string{
+	name := "smoke_test_alarm"
+	expression := "smoke_test_metric>0"
+	alarmDefActions := []string{notificationID}
+	requestBody := models.AlarmDefinitionRequestBody{Name:&name, Expression:&expression, AlarmActions:&alarmDefActions, UndeterminedActions:&alarmDefActions, OkActions:&alarmDefActions}
+	alarmDefinitionResponse, err := monascaclient.CreateAlarmDefinition(&requestBody)
+	if err != nil {
+		fmt.Printf("FAILED - Error creating alarm definition %s\n", err.Error())
+		return ""
+	}
+	fmt.Println("SUCCESS")
+	return alarmDefinitionResponse.ID
+}
+
+func testCreateMetric(value float64) {
+	now := time.Now().Round(time.Millisecond).UnixNano() / (int64(time.Millisecond) / int64(time.Nanosecond))
+	name := "smoke_test_metric"
+	err := monascaclient.CreateMetric(nil, &models.MetricRequestBody{Name:&name, Value:&value, Timestamp:&now})
+	if err != nil {
+		fmt.Printf("FAILED - Error creating metric %s\n", err.Error())
+	}
+	fmt.Println("SUCCESS")
+}
+
+func testWebhookTrigger() {
+	for i := 0; i < 5; i++ {
+		if webhookTriggered {
+			fmt.Println("SUCCESS")
+		}
+		time.Sleep(1 * time.Second)
+	}
+	fmt.Println("FAILED - Did not recieve webhook")
+}
+
+func cleanup(alarmDefinitionID, notificationID string) {
+	if alarmDefinitionID != ""{
+		err := monascaclient.DeleteAlarmDefinition(alarmDefinitionID)
+		if err != nil {
+			fmt.Println("ERROR deleting alarm definition - %s", err.Error())
+		}
+	}
+	if notificationID != "" {
+		err := monascaclient.DeleteNotificationMethod(notificationID)
+		if err != nil {
+			fmt.Println("ERROR deleting alarm definition - %s", err.Error())
+		}
+	}
+}
+
+func main() {
+	// Initialize keystone client and get Monasca endpoint
+	keystoneOpts, err := openstack.AuthOptionsFromEnv()
+
+	if err != nil {
+		fmt.Printf("ERROR setting up keystone client - %s", err.Error())
+		os.Exit(1)
+	}
+
+	monascaURL := os.Getenv("MONASCA_URL")
+
+	if monascaURL == "" {
+		fmt.Println("Monasca URL environment variable must be set")
+		os.Exit(1)
+	}
+
+	keystoneToken, err := getToken(&keystoneOpts)
+
+	if err != nil {
+		fmt.Printf("ERROR getting keystone token - %s", err.Error())
+		os.Exit(1)
+	}
+
+	initializeMonascaClient(keystoneToken, monascaURL)
+
+	fmt.Println("TEST MEASUREMENTS FLOWING")
+	testMeasurementsFlowing()
+
+	// Set Up Webhook server for notifications
+	http.HandleFunc("/", handleWebhook)
+	go http.ListenAndServe(":8080", nil)
+
+	fmt.Println("TEST NOTIFICATION CREATION")
+	notificationID := testCreateNotification("http://127.0.0.1:3455")
+
+	fmt.Println("TEST ALARM DEFINITION CREATION")
+	alarmDefinitionID := testCreateAlarmDefinition(notificationID)
+
+	fmt.Println("TEST METRIC CREATION")
+	testCreateMetric(1)
+
+	fmt.Println("TEST WEBHOOK TRIGGERED")
+	testWebhookTrigger()
+
+	cleanup(alarmDefinitionID, notificationID)
+}
